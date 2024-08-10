48cb1165c958babadf47c61e3a8049df36f023c6
Bittu Sha <144258388+bittu093@users.noreply.github.com>
2024-08-10 09:44:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const SideMenu = document.querySelector("#nav-menu");
+function OpenMenu(){
+    SideMenu.style.left = '0px';
+    SideMenu.style.transaction = '0.3s'
+}
+function CloseMenu(){
+    SideMenu.style.left = '700px';
+}
+
